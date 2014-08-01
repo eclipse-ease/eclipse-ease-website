951e254ec7343565c88f1d67f8589102dcc66948
Christian Pontesegger <christian.pontesegger@web.de>
2014-08-01 09:42:53 +0200

    starting with egit template and modified main page content
    Signed-off-by: Christian Pontesegger <christian.pontesegger@web.de>

---
+++ _projectCommon.php
@@ -0,0 +1,35 @@
+<?php
+
+	# Set the theme for your project's web pages.
+	# See the Committer Tools "How Do I" for list of themes
+	# https://dev.eclipse.org/committers/
+	# Optional: defaults to system theme 
+	$theme = "solstice";
+	
+
+	# Define your project-wide Nav bars here.
+	# Format is Link text, link URL (can be http://www.someothersite.com/), target (_self, _blank), level (1, 2 or 3)
+	# these are optional
+	$Nav->setLinkList(array());
+	$Nav->addNavSeparator("EASE", 	"/ease/");
+	# $Nav->addCustomNav("Download", "/ease/download/", "_self", 3);
+	$Nav->addCustomNav("Documentation", "/ease/documentation/", "_self", 3);
+	$Nav->addCustomNav("Support", "/ease/support/", "_self", 3);
+	$Nav->addCustomNav("Getting Involved", "/ease/developers/", "_self", 3);
+	
+	$pageKeywords	= "eclipse, ease, scripting, javascript";
+	$pageAuthor		= "Christian Pontesegger";
+	$pageTitle 		= "EASE - Eclipse Advanced Scripting Environment";
+
+	$Menu->setMenuItemList(array());
+	$Menu->addMenuItem("Home", "/ease/", "_self");
+	# $Menu->addMenuItem("Download", "/aset/download/", "_self");
+	$Menu->addMenuItem("Documentation", "/ease/documentation/", "_self");
+	$Menu->addMenuItem("Support", "/ease/support/", "_self");
+	$Menu->addMenuItem("Developers", "/ease/developers/", "_self");
+	
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style.css"/>' . "\n\t");
+	$App->AddExtraHtmlHeader('<link rel="stylesheet" type="text/css" href="style2.css"/>' . "\n\t");
+	
+	$App->Promotion = TRUE;
+?>
