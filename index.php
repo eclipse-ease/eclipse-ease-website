b336b03a38d5e8fdf4c7cfbc19b2e3cb09d05226
Christian Pontesegger <christian.pontesegger@web.de>
2014-08-01 11:02:35 +0200

    changed main page to have the menu on the left
    fixed developer gerrit links
    
    Signed-off-by: Christian Pontesegger <christian.pontesegger@web.de>

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	$pageTitle 		= "EASE - Eclipse Advanced Scripting Environment";
 
-	# Paste your HTML content between the EOHTML markers!
-	$html = file_get_contents('_index.html');
+	# provide the whole content
+	# $html = file_get_contents('_index.html');
+
+	# provide the middle section, having the menu on the left hand side
+	$html  = '<div id="midcolumn">';
+	$html .= file_get_contents('_index.html');
+	$html .= "</div>";
 	
 	# Generate the web page
 	$App->generatePage($theme, $Menu, null, $pageAuthor, $pageKeywords, $pageTitle, $html);
