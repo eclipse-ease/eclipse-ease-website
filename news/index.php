142970e27b6c1d9a0dcd31c12e42d7c2beaaeb48
Christian Pontesegger <christian.pontesegger@web.de>
2014-08-16 22:46:45 +0200

    added draft version of "news" page

---
+++ news/index.php
@@ -0,0 +1,25 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	$pageTitle 		= "EASE - News";
+	$pageKeywords	= "Eclipse, EASE, Scripting";
+	$pageAuthor		= "Christian Pontesegger";
+
+	
+	# provide the page content
+	$html = <<<EOHTML
+
+<div id="midcolumn">
+	<dl>
+		<dt><i class="fa fa-fw fa-download bullet">2014/08/16: Nightly update site available</dt>
+		<dd>We have an automated nightly build available which feeds our nightly update site:<br />
+			http://download.eclipse.org/ease/update/nightly</dd>
+	</dl>
+</div>
+
+{$incubation}
+				
+EOHTML;
+	
+	
+	# Generate the web page
+	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
+?>
