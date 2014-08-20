93ba560be3547357dfb00366f179e784d573522e
Christian Pontesegger <christian.pontesegger@web.de>
2014-08-20 19:47:18 +0200

    all update site links are clickable
      prepared for CSS styling, style pending.

--- download/index.php
+++ download/index.php
@@ -18,14 +18,15 @@ function copyToClipboard(text) {
 	
 	<ul class="nobullet">
 		<li><i class="fa fa-fw icon-link bullet"></i>
-			Nightly: <span onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://download.eclipse.org/ease/update/nightly');">http://download.eclipse.org/ease/update/nightly</span></li>
+			Nightly: <span class="p2site" onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://download.eclipse.org/ease/update/nightly');">http://download.eclipse.org/ease/update/nightly</span></li>
 	</ul>
 		
 	<p>External components like additional interpreters can be installed using an external update site:</p>
 	
 	<ul class="nobullet">
 		<li><i class="fa fa-fw icon-link bullet"></i>
-			Nightly: http://topcased-mm.gforge.enseeiht.fr/ease_jython/updates/ease_jython_nightly</li>
+			Nightly: <span class="p2site" onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://topcased-mm.gforge.enseeiht.fr/ease_jython/updates/ease_jython_nightly');">http://topcased-mm.gforge.enseeiht.fr/ease_jython/updates/ease_jython_nightly</span></li>
+			Nightly: </li>
 	</ul>
 </div>
 
