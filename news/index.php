b5eb18669d4df0d8aa03935a4a288218c3c1a8f7
Christian Pontesegger <christian.pontesegger@web.de>
2014-09-08 23:31:33 +0200

    trying 'news aggregator'

--- news/index.php
+++ news/index.php
@@ -35,10 +35,21 @@
 {$incubation}
 				
 EOHTML;
+
+	$html .= '<dl class="news">';
 	
 	foreach (glob(getcwd() . '/*.news') as $file) {
-		$html = $html . file_get_contents($file);
+		$xml = simplexml_load_file($file);
+		
+		$html .= '<dt><i class="fa fa-fw ' . $xml->icon[0] . ' bullet"></i>';
+		$html .= $xml->title[0];
+		$html .= '</dt>';
+		$html .= '<dd>' . $xml->content[0];
+		$html .= '<div class="date">' . date("y/m/d", filemtime($file)) . '</div>';
+		$html .= '</dd>';
 	}
+
+	$html .= '</dl>';
 
 	# Generate the web page
 	$App->generatePage($theme, $Menu, $Nav, $pageAuthor, $pageKeywords, $pageTitle, $html);
