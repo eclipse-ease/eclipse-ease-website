3bf54f4fb9d609853b04027aa05e5db50af0ed6d
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-21 08:14:20 +0200

    release v0.2.0 update

--- download/index.php
+++ download/index.php
@@ -13,11 +13,11 @@
 	<p>Installing the Latest Release</p>
 	<ul class="nobullet">
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			The latest release is v0.1.0 (see Release Notes)</li>
+			The latest release is v0.2.0 (see Release Notes)</li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
 			Main update site: <span class="p2site" onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://download.eclipse.org/ease/update/release');">http://download.eclipse.org/ease/update/release</span> (recommended)</li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.1.0.zip">EASE v0.1.0</a></li>
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.2.0.zip">EASE v0.2.0</a></li>
 	</ul>
 		
 	<p>External components like additional interpreters can be installed using external update sites:</p>
@@ -43,6 +43,8 @@
 	<ul class="nobullet">
 		<li><i class="fa fa-fw icon-link bullet"></i>
 			Release update sites will accumulate releases, so older versions can be obtained from the p2 release update sites directly.</li>
+		<li><i class="fa fa-fw icon-link bullet"></i>			
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.1.0.zip">EASE v0.1.0</a></li>
 	</ul>
 		
 </div>
