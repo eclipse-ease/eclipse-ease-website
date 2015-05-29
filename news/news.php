4e45a9eac02edf619a4234067f3c8533811dd877
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-29 10:15:05 +0200

    first try to publish a news rss feed

---
+++ news/news.php
@@ -0,0 +1,28 @@
+<?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
+	
+	$news = '<?xml version="1.0" encoding="UTF-8" ?>';
+	$news .= '<rss version="2.0">';
+	$news .= '<channel>';
+	$news .= '<title>W3Schools Home Page</title>';
+	$news .= '<link>https://www.eclipse.org/ease/news</link>';
+	$news .= '<description>News feed for the Eclipse EASE framework</description>';
+
+	# aggregate news
+	$dir = glob(getcwd() . '/*.news'); 
+	uasort($dir, function ($a, $b) { return filemtime($b) - filemtime($a);}); 
+
+	foreach ($dir as $file) {
+		$xml = simplexml_load_file($file);
+		
+		$news .= '<item>';
+		$news .= '<title>' . $xml->title[0] . '</title>';
+		$news .= '<link></link>';
+		$news .= '<description>' . $xml->content[0] . '<br />' . date("Y/m/d", filemtime($file)) . '</description>';
+		$news .= '</item>';
+	}
+
+	$news .= '</channel>';
+	$news .= '</rss>';
+	
+	echo $news;
+?>
