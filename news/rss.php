6b7020f96378d945cc1736fc2a4db8ff623c5f9b
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-29 11:23:20 +0200

    next date rss

--- news/rss.php
+++ news/rss.php
@@ -1,12 +1,12 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
-	$news = '<?xml version="1.0" encoding="UTF-8" ?>';
-	$news .= '<rss version="2.0">';
-	$news .= '<channel>';
-	$news .= '<title>Eclipse EASE</title>';
-	$news .= '<link>https://www.eclipse.org/ease/news</link>';
-	$news .= '<description>News feed for the Eclipse EASE framework</description>';
-	$news .= '<language>en</language>';
+	$news = '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
+	$news .= '<rss version="2.0">' . "\n";
+	$news .= '<channel>' . "\n";
+	$news .= '<title>Eclipse EASE</title>' . "\n";
+	$news .= '<link>https://www.eclipse.org/ease/news</link>' . "\n";
+	$news .= '<description>News feed for the Eclipse EASE framework</description>' . "\n";
+	$news .= '<language>en</language>' . "\n";
 
 	# aggregate news
 	$dir = glob(getcwd() . '/*.news'); 
@@ -15,16 +15,16 @@
 	foreach ($dir as $file) {
 		$xml = simplexml_load_file($file);
 		
-		$news .= '<item>';
-		$news .= '<title>' . $xml->title[0] . '</title>';
-		$news .= '<link>https://www.eclipse.org/ease/news/index.php#' . urlencode($xml->title[0]) . '</link>';
-//		$news .= '<pubDate> . date(DATE_RSS, filemtime($file)) . '</pubDate>';
-		$news .= '<description>' . htmlspecialchars($xml->content[0]) . date(DATE_RFC822, filemtime($file))  . '</description>';
-		$news .= '</item>';
+		$news .= '<item>' . "\n";
+		$news .= '<title>' . $xml->title[0] . '</title>' . "\n";
+		$news .= '<link>https://www.eclipse.org/ease/news/index.php#' . urlencode($xml->title[0]) . '</link>' . "\n";
+		$news .= '<pubDate> . date(DATE_RFC822, filemtime($file)) . '</pubDate>' . "\n";
+		$news .= '<description>' . htmlspecialchars($xml->content[0]) . date(DATE_RFC822, filemtime($file))  . '</description>' . "\n";
+		$news .= '</item>' . "\n";
 	}
 
-	$news .= '</channel>';
-	$news .= '</rss>';
+	$news .= '</channel>' . "\n";
+	$news .= '</rss>' . "\n";
 	
 	echo $news;
 ?>
