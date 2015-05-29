b546d598b3fd990e84b395072b02bb791aaeef30
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-29 10:39:25 +0200

    removed old (fixed) news items
    tried to fix rss feed

--- news/index.php
+++ news/index.php
@@ -32,27 +32,6 @@
 	<h3><i class="fa fa-fw fa-bolt"></i> News</h3>
 
 	{$news}
-
-	<dl class="news">
-		<dt><i class="fa fa-fw fa-cogs bullet"></i>
-			Groovy engine preview
-		</dt>
-		<dd>Implemented draft of a Groovy engine. You may find the update site location in the <a href="/ease/download">download section</a>.
-			<div class="date">2014/08/25</div></dd>
-
-		<dt><i class="fa fa-fw fa-cogs bullet"></i>
-			Java 8 Nashorn engine preview
-		</dt>
-		<dd>Just implemented a draft version of the Nashorn JavaScript engine from Java 8. Available on the nightly update site.
-			<div class="date">2014/08/21</div></dd>
-
-		<dt><i class="fa fa-fw fa-download bullet"></i>
-			Nightly update site available
-		</dt>
-		<dd>We have an automated nightly build available which feeds our nightly update site:<br />
-			<span class="p2site" onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://download.eclipse.org/ease/update/nightly');">http://download.eclipse.org/ease/update/nightly</span>
-			<div class="date">2014/08/16</div></dd>
-	</dl>
 </div>
 
 {$incubation}
