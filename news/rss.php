f54f835c7d908850c716dd510e232122fdf71846
Christian Pontesegger <christian.pontesegger@web.de>
2015-05-29 11:46:51 +0200

    removed xml namespace totally

--- news/rss.php
+++ news/rss.php
@@ -1,12 +1,12 @@
 <?php  																														require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/app.class.php");	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/nav.class.php"); 	require_once($_SERVER['DOCUMENT_ROOT'] . "/eclipse.org-common/system/menu.class.php"); 	$App 	= new App();	$Nav	= new Nav();	$Menu 	= new Menu();		include($App->getProjectCommon());    # All on the same line to unclutter the user's desktop'
 	
 	$news = '<?xml version="1.0" encoding="UTF-8" ?>' . "\n";
-	$news .= '<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">' . "\n";
-	$news .= '<atom:channel>' . "\n";
-	$news .= '<atom:title>Eclipse EASE</atom:title>' . "\n";
-	$news .= '<atom:link href="https://www.eclipse.org/ease/news/rss.php" rel="self" type="application/rss+xml" />' . "\n";
-	$news .= '<atom:description>News feed for the Eclipse EASE framework</atom:description>' . "\n";
-	$news .= '<atom:language>en</atom:language>' . "\n";
+	$news .= '<rss version="2.0">' . "\n";
+	$news .= '<channel>' . "\n";
+	$news .= '<title>Eclipse EASE</title>' . "\n";
+	$news .= '<link href="https://www.eclipse.org/ease/news/rss.php" rel="self" type="application/rss+xml" />' . "\n";
+	$news .= '<description>News feed for the Eclipse EASE framework</description>' . "\n";
+	$news .= '<language>en</language>' . "\n";
 
 	# aggregate news
 	$dir = glob(getcwd() . '/*.news'); 
@@ -15,17 +15,17 @@
 	foreach ($dir as $file) {
 		$xml = simplexml_load_file($file);
 		
-		$news .= '<atom:item>' . "\n";
-		$news .= '<atom:title>' . $xml->title[0] . '</atom:title>' . "\n";
-		$news .= '<atom:link>https://www.eclipse.org/ease/news/index.php#' . md5(basename($file)) . '</atom:link>' . "\n";
-		$news .= '<atom:pubDate>' . date(DATE_RSS, filemtime($file)) . '</atom:pubDate>' . "\n";
-		$news .= '<atom:description>' . htmlspecialchars($xml->content[0]) . '</atom:description>' . "\n";
-		$news .= '<atom:guid>https://www.eclipse.org/ease/news/index.php#' . md5(basename($file)) . '</atom:guid>' . "\n";
-		$news .= '</atom:item>' . "\n";
+		$news .= '<item>' . "\n";
+		$news .= '<title>' . $xml->title[0] . '</title>' . "\n";
+		$news .= '<link>https://www.eclipse.org/ease/news/index.php#' . md5(basename($file)) . '</link>' . "\n";
+		$news .= '<pubDate>' . date(DATE_RSS, filemtime($file)) . '</pubDate>' . "\n";
+		$news .= '<description>' . htmlspecialchars($xml->content[0]) . '</description>' . "\n";
+		$news .= '<guid>https://www.eclipse.org/ease/news/index.php#' . md5(basename($file)) . '</guid>' . "\n";
+		$news .= '</item>' . "\n";
 	}
 
-	$news .= '</atom:channel>' . "\n";
-	$news .= '</atom:rss>' . "\n";
+	$news .= '</channel>' . "\n";
+	$news .= '</rss>' . "\n";
 	
 	echo $news;
 ?>
