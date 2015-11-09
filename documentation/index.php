798efad24d791926565f398feb699879bf63058c
Christian Pontesegger <christian.pontesegger@web.de>
2015-11-09 07:53:13 +0100

    added slides from EclipseCon Europe 2015

--- documentation/index.php
+++ documentation/index.php
@@ -17,6 +17,12 @@
 	  <li><a href="https://wiki.eclipse.org/EASE/Modules">Provided modules</a></li>
 	  </ul>
 	</p>
+	<h3><i class="fa fa-fw fa-book"></i> Presentations</h3>
+	<p>
+      <ul class="nobullet">
+	  <li><a href="presentations/eclipsecon_europe_2015/index.html">EclipseCon Europe 2015</a></li>
+	  </ul>
+	</p>
 </div>
 
 {$incubation}
