425624dc0033f00d5675cac73a4c0512b5b36203
Christian Pontesegger <christian.pontesegger@web.de>
2017-09-29 22:59:04 +0200

    Added news for v0.5.0 release

--- download/index.php
+++ download/index.php
@@ -13,11 +13,11 @@
 	<p>Installing the Latest Release</p>
 	<ul class="nobullet">
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			The latest release is v0.4.0 (see Release Notes)</li>
+			The latest release is v0.5.0 (see Release Notes)</li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
 			Main update site: <span class="p2site" onclick="javascript:prompt('Hit Ctrl-c to copy', 'http://download.eclipse.org/ease/update/release');">http://download.eclipse.org/ease/update/release</span> (recommended)</li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.4.0.zip">EASE v0.4.0</a></li>
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.5.0.zip">EASE v0.5.0</a></li>
 	</ul>
 		
 	<p>Nightly builds:</p>
@@ -33,11 +33,13 @@
 		<li><i class="fa fa-fw icon-link bullet"></i>
 			Release update sites will accumulate releases, so older versions can be obtained from the p2 release update sites directly.</li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.1.0.zip">EASE v0.1.0</a></li>
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.4.0.zip">EASE v0.4.0</a></li>
+		<li><i class="fa fa-fw icon-link bullet"></i>			
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.3.0.zip">EASE v0.3.0</a></li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
 			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.2.0.zip">EASE v0.2.0</a></li>
 		<li><i class="fa fa-fw icon-link bullet"></i>			
-			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.3.0.zip">EASE v0.3.0</a></li>
+			p2 Repository for offline installation: <a href=" http://eclipse.org/downloads/download.php?file=/ease/release/ease-p2-v0.1.0.zip">EASE v0.1.0</a></li>
 	</ul>
 		
 </div>
