de5a4bd993a576a778f2ac1efa9564f863e11003
Christian Pontesegger <christian.pontesegger@web.de>
2019-09-05 18:25:51 +0200

    test adapting documentation page
    Change-Id: I943ae6c2de369918371ceaa97f5b6624b1dbbf8b

--- documentation/index.php
+++ documentation/index.php
@@ -17,9 +17,22 @@
 	  <li><a href="https://wiki.eclipse.org/EASE/Modules">Provided modules</a></li>
 	  </ul>
 	</p>
-	<h3><i class="fa fa-fw fa-book"></i> Presentations</h3>
+	<h3><i class="fa fa-fw fa-video-camera"></i> Presentations</h3>
 	<p>
       <dl class="nobullet">
+      	<dt>EASEy IDE extensions without Plugins</dt>
+      	<dd>
+      		<p>Eclipse EASE allows you to extend the IDE by writing scripts in Javascript, Python or Groovy without having to create a Plugin. No compilation and restarts needed, the changes to your scripts are immediately working in the running IDE.</p>
+			<p>I will show how to get started with Eclipse EASE and give a short overview of the possibilities. The talk will be concluded with examples of real world scripts which I am using in our IDE to make day-to-day development tasks easier.</p>
+
+      		Speaker: Rabea Gransberger<br />
+      		Location: EclipseCon Europe 2018<br />
+      		<i class="fa fa-video-camera"> <a href="https://youtu.be/HfRMQ3oDs-g">watch on youtube</a> 
+      	</dd>
+      	
+      	<dt>EclipseCon Europe 2019</dt>
+      	<dd>SysBox: An Eclipse Story (<a href="https://youtu.be/0B2sttEdHio">watch on youtube</a>)</dd>
+      
       	<dt>EclipseCon Europe 2018</dt>
       	<dd>EASEy IDE extensions without Plugins (<a href="https://youtu.be/HfRMQ3oDs-g">watch on youtube</a>)</dd>
       	<dd>SysBox: An Eclipse Story (<a href="https://youtu.be/0B2sttEdHio">watch on youtube</a>)</dd>
@@ -28,7 +41,7 @@
       	<dd>And the people said, "Let there be scripts" (<a href="https://youtu.be/r7SSf5DMihQ">watch on youtube</a>)</dd>
 
       	<dt>EclipseCon France 2017</dt>
-      	<dd><i class="fa fa-video-camera"></i>How EASE unleashes the scientific power of Airbus' engineers in Eclipse (<a href="https://www.youtube.com/watch?v=4ZkV3MDxabc">watch on youtube</a>)</dd>
+      	<dd></i>How EASE unleashes the scientific power of Airbus' engineers in Eclipse (<a href="https://www.youtube.com/watch?v=4ZkV3MDxabc">watch on youtube</a>)</dd>
 
       	<dt>EclipseCon Europe 2016</dt>
       	<dd>EASE-ily make the most of Eclipse with Python (<a href="https://youtu.be/bhhypyRAPp0">watch on youtube</a>)</dd>
@@ -42,6 +55,11 @@
 
       	<dt>EclipseCon Europe 2013</dt>
       	<dd>Scripting, baby! (<a href="https://youtu.be/FQw3eNYTgfI">watch on youtube</a>)</dd>
+
+      	<dt>Further videos</dt>
+      	<dd>Scripting in Eclipse RCP-based applications (<a href="https://www.youtube.com/watch?v=32TqigGZSGg">watch on youtube</a>)</dd>
+      	<dd>Eclipse EASE (<a href="https://www.youtube.com/watch?v=PDAnK0lkANg&t=51s">watch on youtube</a>)</dd>
+      	<dd>Easy scripting, easy modeling (<a href="https://www.youtube.com/watch?v=mSjxwVRIBf0">watch on youtube</a>)</dd>
 	  </dl>
 	</p>
 </div>
