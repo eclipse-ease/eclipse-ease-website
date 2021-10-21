00e37c8d641bcfa304aaf8cff1f84b539f942489
Christian Pontesegger <christian.pontesegger@web.de>
2021-10-21 09:00:49 +0200

    Bug 576792: Update target platform to 2021-09
      update compatibility matrix on download page
    
    Change-Id: I1e7cffef726e7de5fdda77f9f49b80054df961f1

--- download/index.php
+++ download/index.php
@@ -57,7 +57,7 @@ The matrix below is not complete. In case you do use something we do not track, 
 		<tr>
 			<th>2021-09</th>
 			<td><a href='https://ci.eclipse.org/ease/job/ease.build.core/lastBuild'><img src='https://ci.eclipse.org/ease/job/ease.build.core/badge/icon?style=plastic&subject=Tests'></a></td>
-			<td><span title="UnitTests pass"><i class="fa fa-check green" /></span></td>
+			<td></td>
 			<td></td>
 			<td></td>
 			<td></td>
